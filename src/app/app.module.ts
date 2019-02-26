fc60a8eae5d4b05fb559dd78904f5593aa122b4d
Gustavo <gustavo.simoes@hotmail.com.br>
2019-02-26 13:15:24 -0300

    Creation the component menu-left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { MenuLeftComponent } from './components/template/menu-left/menu-left.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuLeftComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
